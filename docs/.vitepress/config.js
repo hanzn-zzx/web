d7a7511dd5d16218ad7cf81a0e5cde4ed85420c4
hanzn-zzx <1394753132@qq.com>
2023-04-05 18:06:41 +0800

    set up common website

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,22 @@
-module.exports = {
-	  title: 'hanzn-zzx的网站',
-	  description: 'hanzn-zzx的个人网站（静态）'
-          }
+export default {
+	title: 'hanzn-zzx的个人首页', 
+	description: 'hanzn-zzx的个人静态网站', 
+	lang: 'zh-CN',
+	themeConfig: {
+		siteTitle: 'hanzn-zzx',
+		nav: [
+			{ text: 'deepin论坛个人首页', link: 'https://bbs.deepin.org/user/284017'}
+		],
+		socialLinks: [
+			{ icon: 'github', link: 'https://github.com/hanzn-zzx' }
+		],
+		footer: {
+			message: 'hanzn-zzx',
+		}
+	},
+	sidebar: [
+		{ text: '首页', link: '/' },
+	],
+	outlineTitle: '页面目录'
+}
+
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,1 @@
-# Hello VitePress
+#hanzn-zzx的个人静态站点
